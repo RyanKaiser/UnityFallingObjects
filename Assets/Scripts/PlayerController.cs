9684c82678cdf946e9b43c6acb7928de2e1bac20
BBVR <build-bot@blackbox-vr.com>
2025-08-08 00:54:17 -0700

    Add animations - jump, run, land

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,24 +5,39 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Animator _animator;
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _jumpForce = 10f;
 
 
     private bool _isGrounded;
+    private bool _isFacingRight = true;
     private float horizontalMovement;
     public event Action OnPlayerDied;
 
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+        _animator.SetFloat("velocityX", Mathf.Abs(horizontalMovement));
+        if (horizontalMovement > 0 && !_isFacingRight)
+        {
+            _isFacingRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (horizontalMovement < 0 && _isFacingRight)
+        {
+            _isFacingRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && _isGrounded)
         {
             _rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            _animator.SetBool("grounded", false);
+            _isGrounded = false;
         }
     }
 
@@ -33,24 +48,28 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+
         if (other.collider.CompareTag("FallingObject"))
         {
-            OnPlayerDied?.Invoke();
-            Destroy(gameObject);
+            // OnPlayerDied?.Invoke();
+            // Destroy(gameObject);
         }
 
-        if (other.collider.CompareTag("Ground"))
-        {
-            _isGrounded = false;
-        }
-    }
-
-    private void OnColliderExit2D(Collision2D other)
-    {
         if (other.collider.CompareTag("Ground"))
         {
             _isGrounded = true;
+            _animator.SetBool("grounded", true);
         }
     }
+
+    // private void OnColliderExit2D(Collision2D other)
+    // {
+    //     if (other.collider.CompareTag("Ground"))
+    //     {
+    //         _isGrounded = true;
+    //         _animator.SetBool("grounded", true);
+    //
+    //     }
+    // }
 
 }
